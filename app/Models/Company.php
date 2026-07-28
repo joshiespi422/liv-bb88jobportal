6ac5ad7696715ea2d0386a3b4b6eb9aeba120a2b
joshi <jespadilla422@gmail.com>
2026-07-28 18:26:46 +0800

    feat(migration, model): add slug column in companies table, incude in seeder, and configure in fillable

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -12,6 +12,7 @@ class Company extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'tin',
         'address',
     ];
--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -16,26 +16,31 @@ class CompanySeeder extends Seeder
         $companies = [
             [
                 'name' => 'MIGS Masters Institute For Graphics Inc.',
+                'slug' => 'migs-masters-institute-for-graphics-inc',
                 'tin' => '010-326-819-000',
                 'address' => 'Penthouse 1 2F, Plaza Victoria Sto. Rosario Street Santo Domingo Angeles City 2009',
             ],
             [
                 'name' => 'BB88 Advertising And Digital Solutions Inc.',
+                'slug' => 'bb88-advertising-and-digital-solutions-inc',
                 'tin' => '754-728-161-000',
                 'address' => 'Unit D 2/F Plaza Victoria Sto. Rosario St. Santo Domingo Angeles City 2009',
             ],
             [
                 'name' => 'Biorganism Corporation',
+                'slug' => 'biorganism-corporation',
                 'tin' => '600-875-090-00000',
                 'address' => '529 Unit D 2/F Plaza Victoria Sto. Rosario Santo Domingo 2009 Angeles City Pampanga Philippines',
             ],
             [
                 'name' => 'MHR Phils Construction Corporation',
+                'slug' => 'mhr-phils-construction-corporation',
                 'tin' => '606-495-245-00000',
                 'address' => '529 2/F Plaza Victoria Sto. Rosario St. Santo Domingo 2009 Angeles City Pampanga Philippines',
             ],
             [
                 'name' => 'Regreen Life Inc.',
+                'slug' => 'regreen-life-inc',
                 'tin' => '600-874-545-00000',
                 'address' => '529 Unit D 2/F Plaza Victoria Rosario St. Santo Domingo 2009 Angeles City Pampanga Philippines',
             ],
